4a144407c34e302d7f91c6268b78c7318bb8d634
Pedro Alves <pedromsilvaalves@gmail.com>
2019-05-16 20:37:19 -0300

    Tarefa 4 - Remodelagem da arquitetura da Factory

---
+++ TI.Desempenho.Esportivo/Factory/Interface/InterfaceFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TI.Desempenho.Esportivo.Service.Interface;
+
+namespace TI.Desempenho.Esportivo.Factory.Interface
+{
+    public interface InterfaceFactory
+    {
+        InterfaceCriar Criar();
+    }
+}
