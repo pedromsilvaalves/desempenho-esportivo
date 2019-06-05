3b33b05588ad8594aa16c332b395d11fd0ce544b
Pedro Alves <pedromsilvaalves@gmail.com>
2019-06-05 00:40:43 -0300

    Tarefa-15: Atualização da Classe Time para suportar sistema de pontos e buscar dados dos Jogadores.

--- TI.Desempenho.Esportivo/Service/Time.cs
+++ TI.Desempenho.Esportivo/Service/Time.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TI.Desempenho.Esportivo.Model;
 
 namespace TI.Desempenho.Esportivo.Service
 {
@@ -60,14 +61,90 @@ namespace TI.Desempenho.Esportivo.Service
 
         #region Metodos
 
-        public void addJogador(String nome, int posicao, int camisa)
+        public void AddJogador(String nome, int posicao, int camisa)
         {
             if (numJogador < 44)
             {
-                Jogador aux = new Jogador(nome, posicao, camisa);
-                this.jogadores[numJogador] = aux;
+                Jogador novoJogador = new Jogador(nome, posicao, camisa);
+                this.jogadores[numJogador] = novoJogador;
                 numJogador++;
             }
+        }
+
+        public double PontosTotaisTime()
+        {
+            double pontosTotais = 0;
+            foreach(var jogador in jogadores)
+            {
+                pontosTotais += jogador.pontosTotais();
+            }
+            return pontosTotais;
+        }
+
+        public double MediaPontosTime()
+        {
+            if(numJogador != 0)
+                return PontosTotaisTime() / numJogador;
+            return 0;
+        }
+
+        public List<AvaliacaoJogadorModel> RelatorioJogadores()
+        {
+            var relatorio = new List<AvaliacaoJogadorModel>();
+            var media = MediaPontosTime();
+
+            foreach(var jogador in _jogadores)
+            {
+                if(jogador.pontosTotais() == media)
+                {
+                    relatorio.Add(new AvaliacaoJogadorModel()
+                    {
+                        nome = jogador.nome,
+                        numCamisa = jogador.camisa,
+                        nomPosicao = jogador.posicao.nomPosicao,
+                        pontos = jogador.pontosTotais(),
+                        status = "Dentro da média."
+                    });
+                }
+                else if(jogador.pontosTotais() < media)
+                {
+                    relatorio.Add(new AvaliacaoJogadorModel()
+                    {
+                        nome = jogador.nome,
+                        numCamisa = jogador.camisa,
+                        nomPosicao = jogador.posicao.nomPosicao,
+                        pontos = jogador.pontosTotais(),
+                        status = "Abaixo da média."
+                    });
+                }
+                else
+                {
+                    relatorio.Add(new AvaliacaoJogadorModel()
+                    {
+                        nome = jogador.nome,
+                        numCamisa = jogador.camisa,
+                        nomPosicao = jogador.posicao.nomPosicao,
+                        pontos = jogador.pontosTotais(),
+                        status = "Acima da média."
+                    });
+                }
+            }
+            return relatorio;
+        }
+
+        public Jogador ProcurarJogadorMaiorPontuacao()
+        {
+            Jogador jogadorMaiorPontuacao = null;
+            double maiorPontuacao = double.MinValue;
+            foreach(var jogador in _jogadores)
+            {
+                if(jogador.pontosTotais() > maiorPontuacao)
+                {
+                    jogadorMaiorPontuacao = jogador;
+                    maiorPontuacao = jogador.pontosTotais();
+                }
+            }
+            return jogadorMaiorPontuacao;
         }
 
         #endregion
